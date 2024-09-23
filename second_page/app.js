d8ffa7139e1785245e3a988bc5ab666e7df7a751
Gev2209 <mirzoyan.gevorg.23@gmail.com>
2024-09-24 00:23:42 +0400

    Add chat

--- second_page/app.js
+++ second_page/app.js
@@ -6,9 +6,11 @@ const articles_page = document.getElementById('articles');
 const inputNamme = document.querySelector('.name_pic');
 const btn_message = document.querySelector('.btn');
 const history_text = document.querySelector('.histoy_list_li');
+const text_contents = document.querySelector('.history_text_content');
 
 
 let count = 0;
+const list_texts = [];
 
 const counters = {
     0:'none',
@@ -31,19 +33,21 @@ const handle_picture_decrement = () => {
 }
 
 const handle_btn_text = () => {
-    count++
-    history_text.innerText = inputNamme.value
-    console.log(history_text,'history_text');
-    
-    console.log(count);
-    
-    // history_text.innerText = inputNamme.value
-    // console.log(inputNamme.value);    
+        const name = 'Gevorg';
+        list_texts.push(inputNamme.value);
+        for (let i = 0; i < 1; i++) {
+        const elementUl = document.createElement('ul');
+        const elementLi = document.createElement('li');
+        console.log(elementUl,'>>>>elementUl');
+        elementLi.textContent = `${name}: ${inputNamme.value}`;
+        elementUl.appendChild(elementLi)
+        text_contents.appendChild(elementUl)
+        }
 }
-
 
 btn_message.addEventListener('click',handle_btn_text);
 arrow_right.addEventListener('click',handle_picture_increment);
 arrow_left.addEventListener('click',handle_picture_decrement);
+
 
 
